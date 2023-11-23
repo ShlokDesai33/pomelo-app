a6af9440366bd30df3af0b7fca59c7436eb2017f
Shlok Desai <shlokdesai33@gmail.com>
2023-11-23 14:08:40 -0600

    docker compose fix

--- db.ts
+++ db.ts
@@ -2,7 +2,7 @@ import pg from "pg";
 
 export async function getClient() {
   const client = new pg.Client(
-    "postgres://postgres:password@localhost:5432/postgres"
+    "postgres://postgres:password@postgres:5432/postgres"
   );
 
   await client.connect();
